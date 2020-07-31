eeef844541f602599a19f375a39a375120640384
DJ <dj@walker.dev>
2020-07-31 17:34:23 -0400

    test preview mode fastglob

--- pages/test.tsx
+++ pages/test.tsx
@@ -3,10 +3,12 @@ import { readJsonFile } from 'utils/getJsonPreviewProps'
 import { MarkdownContent } from 'components/layout'
 import { useCMS } from 'tinacms'
 import fs from 'fs'
+const fg = require('fast-glob')
 
+//@ts-ignore
 const path = __non_webpack_require__('path')
 
-export default function TestPage({ guidesIndex, packagesIndex }) {
+export default function TestPage({ guidesIndex, packagesIndex, guideFiles }) {
   const cms = useCMS()
   return (
     <div>
@@ -28,17 +30,25 @@ export default function TestPage({ guidesIndex, packagesIndex }) {
         Exit preview mode
       </button>
       <hr />
-      <pre>{JSON.stringify(guidesIndex)}</pre>
-      <pre>{JSON.stringify(packagesIndex)}</pre>
+      <pre>{JSON.stringify(guideFiles, null, 2)}</pre>
+      <pre>{JSON.stringify(guidesIndex, null, 2)}</pre>
+      <pre>{JSON.stringify(packagesIndex, null, 2)}</pre>
     </div>
   )
 }
 
 export const getStaticProps = async ({ preview }) => {
+  // the following line will cause all content files to be available in a serverless context
+  path.resolve('./content/')
+
   const guides = path.resolve(process.cwd(), './content/guides/index.md')
   const pkgs = path.resolve(process.cwd(), './content/packages.json')
+  const guideFiles = await fg(
+    `${path.resolve('./content/guides')}/**/meta.json`
+  )
   return {
     props: {
+      guideFiles,
       withoutGithub: true,
       preview: !!preview,
       guidesIndex: await fs.readFileSync(guides, 'utf8'),
